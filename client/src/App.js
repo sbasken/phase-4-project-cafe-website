2b6faef5bde5a10c38488102b3c6f4a92d7115d0
Saki <sbasken@gmail.com>
2023-04-18 13:53:39 -0700

    created components file and routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
 import Home from './components/home';
+import Navbar from '../components/navbar';
+import Menu from '../components/menu';
+import Order from '../components/order';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div >
-      <header >
-        <Home />
-      </header>
+      <div className="row">
+        <Navbar />
+      </div>
+      <div className="row">
+        <Routes>
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/home" element={<Menu />} />
+          <Route exact path="/home" element={<Order />} />
+        </Routes>
+      </div>
     </div>
   );
 }
