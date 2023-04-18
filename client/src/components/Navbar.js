1eef4d1e9cfa8b665ad9883cb5720070c7639ee4
Saki <sbasken@gmail.com>
2023-04-18 15:16:33 -0700

    set up navbar routes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className="ui menu">
-          <div className="header item">Component Cafe</div>
-           <a className="item">Home</a>
-            <a className="item">Menu</a>
-            <a className="item">Orders</a>
-            <a className="item">About</a>
+        <div className="header item">Component Cafe</div>
+            <a className="item" href="/home">Home</a>
+            <a className="item" href="/menu">Menu</a>
+            <a className="item" href="/orders">Orders</a>
+            <a className="item" href="/about">About</a>
         <div className="right menu">
             <a className="item">Login</a>
         </div>
-        </div>
+    </div>
   )
 }
 
