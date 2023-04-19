f0f11bc23619db7ffacecf046659302718079685
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-04-18 17:04:51 -0700

    login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Segment, Grid, Form, Divider, Button } from 'semantic-ui-react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <Segment secondary>
+      <Grid >
+        <Grid.Column verticalAlign='middle' >
+          <Form>
+          <h1>Login to Place an Order</h1>
+          <br/>
+            <Form.Field >
+              <label>Username:</label>
+              <Form.Input
+                type="text"
+                placeholder="Username"
+              />
+            </Form.Field>
+            <br/>
+            <Form.Field>
+              <label>Password:</label> 
+              <Form.Input
+                type="password"
+                placeholder="Password"
+              />
+              </Form.Field>
+              <br/>
+              <Button 
+              className='ui button'
+              >Log In</Button>
+              <br/>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    </Segment>
+    </>
   )
 }
 
