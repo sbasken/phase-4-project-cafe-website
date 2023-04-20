71764e7f08f585e4e5118a69f1dd6685ec62aaaa
Saki <sbasken@gmail.com>
2023-04-19 19:44:17 -0700

    fix to resolve confilicts

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Order from './components/Orders';
 import About from './components/About';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import EditItem from './components/EditItem';
 import Cart from './components/Cart';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
