896b3c69ae7d0fbfad6abe7a490f355e1a1dc32d
Saki <sbasken@gmail.com>
2023-04-20 13:18:23 -0700

    Add prefilled form to edititem and updated menu/id endpoint functions

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -3,7 +3,6 @@ import { Card, Icon, Image, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const ItemCard = ({ item, currentUser, onDeleteItem }) => {
-    console.log(currentUser.customer)
 
     const addToCart = (e) => {
       console.log(e)
@@ -24,14 +23,22 @@ const ItemCard = ({ item, currentUser, onDeleteItem }) => {
         })
         .then(res => {
             if (res.ok) {
-                res.json().then(() => onDeleteItem(item.id))
+                res.json().then(() => {
+                  console.log(`Deleting item with id ${item.id}`)
+                  onDeleteItem(item.id)
+                })
         }}
         )
     }
 
   return (
-    <Card>
-    <Image src='https://images.seattletimes.com/wp-content/uploads/2019/11/11122019_burgers_144441.jpg?d=780x520' wrapped ui={false} />
+    <Card style={{ height: '400px' }}>
+      <div style={{ height: '60%', overflow: 'hidden' }}>
+          <Image
+              src={item.img_url}
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+      </div>
     <Card.Content>
       <Card.Header>{item.name}</Card.Header>
       <Card.Meta>
