ef2a9f35a2f65014b5f7c6476082c7cd14788a4e
sbasken <120995506+sbasken@users.noreply.github.com>
2023-04-21 09:56:42 -0700

    Fix conflict

--- client/src/App.js
+++ client/src/App.js
@@ -97,6 +97,7 @@ function App() {
       <div className="row">
         <Routes>
           <Route path="/home" element={<Home currentReceipt={currentReceipt}/>} />
+          <Route path="/menu/:id" element={<EditItem handleUpdateItem={updateItem}/>} />
           <Route path="/menu" element={<MenuPage 
                                           currentUser={currentUser} 
                                           currentReceipt={currentReceipt}
@@ -104,7 +105,6 @@ function App() {
                                           filteredItems={filteredItems}
                                           deleteItem={deleteItem}
                                           />} />
-          <Route path="/menu/:id" element={<EditItem handleUpdateItem={updateItem}/>} />
           <Route path="/orders" element={<Order />} />
           <Route path="/about" element={<About />} />
           <Route path="/newitem" element={<NewItem onAddItem={onAddItem}/>} />
