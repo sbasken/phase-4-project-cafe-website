ef60fd92dc124473e42ad046042a7fa577681c92
Saki <sbasken@gmail.com>
2023-04-24 14:00:29 -0700

    Add . before models in app.py

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
 from .config import app, api, db
-from models import User, MenuItem, OrderItem, Receipt
+from .models import User, MenuItem, OrderItem, Receipt
 
 class Signup(Resource):
 
