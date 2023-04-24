5753fc3fbc81885e21973e3a78165cd6389e7226
Saki <sbasken@gmail.com>
2023-04-24 14:38:20 -0700

    Add line to update in app.py

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from flask import jsonify, make_response, request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
+
 from .config import app, api, db
 from .models import User, MenuItem, OrderItem, Receipt
 
