37c2076dd7c5e4ab610904ad5a250105cc23716a
Saki <sbasken@gmail.com>
2023-04-24 16:46:28 -0700

    Set up for deployment

--- server/config.py
+++ server/config.py
@@ -7,7 +7,15 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-app = Flask(__name__)
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(
+    __name__,
+    static_folder='../client/build',
+    template_folder='../client/build'
+    )
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
